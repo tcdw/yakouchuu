3368c25eeea1e0eb2a7e51ccf32ff5b11a82f1a7
tcdw <tcdw2011@gmail.com>
2019-05-22 00:07:50 +0800

    基本完工了（ry

--- ykc2mml.js
+++ ykc2mml.js
@@ -14,8 +14,9 @@ if (argv._.length < 1 || argv.h || argv.help) {
 const entryPtr = typeof argv.entryptr === 'undefined' ? 0x1C00 : Number(argv.entryptr);
 const spcPath = path.resolve(process.cwd(), argv._[0]);
 const spc = fs.readFileSync(spcPath);
+const brrNameMap = argv.brrnamemap ? fs.readJSONSync(argv.brrnamemap, { encoding: 'utf8' }) : {};
 const ast = parser(spc, entryPtr);
-const mml = amml(ast, false);
-const finalTxt = amk(mml);
+const mml = amml(ast, spc, false, typeof argv.doubletick === 'undefined' ? 1 : Math.floor(Number(argv.doubletick)));
+const finalTxt = amk(mml, ast, spc, spcPath, brrNameMap);
 fs.writeFileSync(`${spcPath}.json`, beautify(ast, null, 2, 80), { encoding: 'utf8' });
 fs.writeFileSync(`${spcPath}.txt`, finalTxt, { encoding: 'utf8' });
