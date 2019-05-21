323332ab3e70ff2e7b235a3cecdc4a2cd70d942b
tcdw <tcdw2011@gmail.com>
2019-05-21 12:33:28 +0800

    美化 json 输出

--- ykc2mml.js
+++ ykc2mml.js
@@ -1,6 +1,7 @@
 const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs-extra');
 const path = require('path');
+const beautify = require('json-beautify');
 const parser = require('./lib/parser');
 
 if (argv._.length < 1 || argv.h || argv.help) {
@@ -12,4 +13,4 @@ const entryPtr = typeof argv.entryptr === 'undefined' ? 0x1C00 : Number(argv.ent
 const spcPath = path.resolve(process.cwd(), argv._[0]);
 const spc = fs.readFileSync(spcPath);
 const ast = parser(spc, entryPtr);
-fs.writeJSONSync(`${spcPath}.json`, ast, { encoding: 'utf8', spaces: 2 });
+fs.writeFileSync(`${spcPath}.json`, beautify(ast, null, 2, 80), { encoding: 'utf8' });
